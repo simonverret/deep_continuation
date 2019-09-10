26cf79b4a8a8066d47661d9b1736312a4d5773e3
RezaNourafkan <43887512+RezaNourafkan@users.noreply.github.com>
2019-09-10 10:46:46 -0400

    Add files via upload
    After loading "gcc/8.3.0", you can compile the code by typing 
    $  make
    
    The executable file name is:
    generateDataCond
    
    At the top of the main.cpp, you can change the parameters. For example, the number of conductivity data is controlled by "numData" parameters. "numIntervalOmega" is the number of mesh point on real frequency (or output size in NN). The number of boson Matsubara frequency (or input size in NN) is given by 12.8*beta, where "beta" denotes the inverse temperature. 
    
    The total spectral weight is stetted to \pi, hence, \Pi(\nu_0=0) is one. This condition can be lifted by setting the "normalization" parameter to "false" at the top of the main.cpp. 
    
    After running, the code produces and "Pi.csv" and  "SigmaRe.csv". It also write the first generated data in the "Pi_sample.csv" and SigmaRe_sample.csv". You can plot these files using gnuplot to visualize them. The freq. mesh on the real axis in a non-uniform one with finer mesh at low frequency where narrow Drude peak is located.

---
+++ generateDataConductivity/OpticalConductivity.hpp
@@ -0,0 +1,52 @@
+//
+//  OpticalConductivity.hpp
+//  generateDataConductivity
+//
+//  Created by Reza Nourafkan on 2019-08-23.
+//  Copyright © 2019 Reza Nourafkan. All rights reserved.
+//
+
+#ifndef OpticalConductivity_hpp
+#define OpticalConductivity_hpp
+
+#include <vector>
+
+class OpticalConductivity{
+friend void csvWrite(const std::vector<OpticalConductivity> &op, const unsigned int& maxNumDrude, const unsigned int& maxNumDrudeLorentz);
+public:
+    OpticalConductivity(int numDrude, std::vector<double> plasmaFrequencies, std::vector<double> scatteringRates, int numDrudeLorentz, std::vector<double> positions, std::vector<double> widths, std::vector<double> strengths);
+    OpticalConductivity(const OpticalConductivity &s);
+    OpticalConductivity &operator=(const OpticalConductivity &s);
+    ~OpticalConductivity();
+    //
+    static void initFrequencyMesh(const unsigned int numInterval, const double bandWidth);
+    static void initBosonicMatsubaraFrequencyMesh(const double beta, const unsigned int nMax);
+    double calculateOpticalConductivity(const double &omega);
+    double kernel(const double &omega, const double &nu);
+    void setNormalization(const double &wp);
+    void calculateOpticalConductivity();
+    void calculateOpticalPolarization(const double& bandWidth);
+    void csvWrite();
+    //
+private:
+    unsigned int m_numDrude;
+    std::vector<double> m_plasmaFrequencies;
+    std::vector<double> m_scatteringRates;
+    //
+    unsigned int m_numDrudeLorentz;
+    std::vector<double> m_positions;
+    std::vector<double> m_widths;
+    std::vector<double> m_strengths;
+    //
+    static std::vector<double> frequencyMesh;
+    static std::vector<double> BosonicMatsubaraFrequencyMesh;
+    //
+    double m_normalization;
+    std::vector<double> m_sigma;
+    std::vector<double> m_Pi;
+    //
+};
+
+void csvWrite(const std::vector<OpticalConductivity> &op);
+
+#endif /* OpticalConductivity_hpp */
