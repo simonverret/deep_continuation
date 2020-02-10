f884409f50a496c1245b541364b5cab188a91c84
Simon Verret <verret.simon@gmail.com>
2020-02-10 15:01:45 -0500

    improved the readability of the arg parser code

--- mlp/utils.py
+++ mlp/utils.py
@@ -4,54 +4,39 @@ import os
 import json
 import argparse
 
-def parse_file_and_command(default_dict, help_dict, params_file = 'params.json', argv=None):
+def parse_file_and_command(default_dict, help_dict, argv=None):
     parser = argparse.ArgumentParser()
 
-    ## WARNING: ENABLING PARSING OF THE PARAMS FILENAME REMOVES THE HELP
+    params_file = 'params.json'
+    ## UNCOMMENT TO PARSE params_file (REMOVES THE HELP) before else
     # parser.add_argument('--file', type=str, default=default_parameters['file'], help=help_str['file'])
     # params_file = parser.parse_known_args()[0].file
-    params_dict = {}
-    if (params_file is not None):
-        if os.path.exists(params_file):
-            with open(params_file) as f:
-                params_dict = json.load(f)
-        else:
-            print("warning: input file '"+params_file+"' not found") 
-        
+    
+    if os.path.exists(params_file):
+        with open(params_file) as f:
+            params_dict = json.load(f)
+    else:
+        print("warning: input file '"+params_file+"' not found") 
+        params_dict = {}
 
     for name, default in default_dict.items():
         
         ## replace the defaults by the json file content
         try: default = params_dict[name]
         except KeyError: pass
+        try: help_str = help_dict[name]
+        except KeyError: help_str = 'no help available'
 
-        ## build the kwargs that will be passed to parser.add_argument()
-        kwargs = {}
-        kwargs['default'] = default
-        try:
-            kwargs['help'] = help_dict[name]
-        except KeyError:
-            kwargs['help'] = 'sorry, no help string available'
-        if   type(default) is list:
-            kwargs['nargs']  = '+' 
-            kwargs['type']   = type(default[0])
+        if   type(default) is list:   
+            parser.add_argument('--'+name, nargs='+', type=type(default[0]), default=default, help=help_str)
         elif type(default) is bool:
-            kwargs['action'] = 'store_true'
+            parser.add_argument('--'+name, action='store_true', default=default, help=help_str)
+            parser.add_argument('--no_'+name, dest=name, action='store_false', default=default, help='disables '+name)  
         else:
-            kwargs['type']   = type(default)
-        
-        ## create the script argument
-        parser.add_argument('--'+name, **kwargs)
-
-        ## create the anti-argument (for bool arguments)
-        if type(default) is bool:
-            parser.add_argument(
-                '--no_'+name, dest=name, action='store_false', 
-                default=default, help='disables '+name
-            )  
+            parser.add_argument('--'+name, type=type(default), default=default, help=help_str)
     
-    # using parser.parse_known_args()[0] instead of parser.parse_args() 
-    # preserve compatibility with jupyter in vscode
+    # using parser.parse_known_args()[0] instead of parser.parse_args() preserve
+    # compatibility with jupyter in vscode
     return parser.parse_known_args()[0]
 
 class ObjectView():
