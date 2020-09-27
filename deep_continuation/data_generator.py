removed old file management, corrected normalization

deep_continuation/data_generator.py
@@ -196,9 +196,9 @@ def infer_scale_plot(Pi, sigma, filename=None):
 
 
 class DataGenerator():
-    def __init__(self, Nwn=128, Nw=512, beta=[10], wmax=20, rescale=0.0, **kwargs):
+    def __init__(self, Nwn=128, Nw=512, beta=[10.0], wmax=20, rescale=0.0, **kwargs):
         self.Nwn = Nwn
-        self.Nn = Nw
+        self.Nw = Nw
         self.beta = beta
         self.wmax = wmax
         self.rescale = rescale
@@ -209,19 +209,18 @@ class DataGenerator():
     def generate_batch(self, size):
         Pi = np.zeros((len(self.beta), size, self.Nwn))
         betas = np.zeros((len(self.beta), size))
-        sigma = np.zeros((size, self.Nn))
-        sigma_r = np.zeros((size, self.Nn))
+        sigma = np.zeros((size, self.Nw))
+        sigma_r = np.zeros((size, self.Nw))
         wmaxs = np.zeros(size)
 
         for i in range(size):
             if (i == 0 or (i+1)%(max(1, size//100)) == 0): print(f"{i+1}/{size}")
             sigma_func, pi_func = self.generate_functions()
-
             
             if self.rescale > SMALL:
                 s = INF**2*pi_integral(INF, sigma_func, grid_end=self.wmax)
                 wmax = np.cbrt(s) * self.rescale
-                omega = np.linspace(0, wmax, self.Nn)
+                omega = np.linspace(0, wmax, self.Nw)
                 sigma_r[i] = sigma_func(omega)
                 wmaxs[i] = wmax
             else:
@@ -232,7 +231,7 @@ class DataGenerator():
                 Pi[b,i] = pi_func(omega_n)
                 betas[b,i] = beta
 
-            omega = np.linspace(0, self.wmax, self.Nn)
+            omega = np.linspace(0, self.wmax, self.Nw)
             sigma[i] = sigma_func(omega)
 
         return Pi, sigma, betas, wmaxs, sigma_r
@@ -245,10 +244,10 @@ class DataGenerator():
         np.savetxt(sigma_path, sigma, delimiter=',')
 
         for b, beta in enumerate(self.beta[1:]):
-            new_path = pi_path.replace(".csv", f"_beta_{beta}.csv")
+            new_path = pi_path.replace(".csv", f"_beta_{beta:f}.csv")
             np.savetxt(new_path, Pi[b], delimiter=',')
         if self.rescale:
-            new_path = sigma_path.replace(".csv", f"_scaled_{self.rescale}.csv")
+            new_path = sigma_path.replace(".csv", f"_scaled_{self.rescale:f}.csv")
             np.savetxt(new_path, sigma_r, delimiter=',')
         if wmaxs_path:
             np.savetxt(wmaxs_path, wmaxs, delimiter=',')
@@ -256,7 +255,9 @@ class DataGenerator():
     def plot(self, size, name=None, basic=True, scale=False, infer=False):
         Pi, sigma, betas, wmaxs, sigma_r = self.generate_batch(size)
         print(f" true scales:\n  betas =\n{betas}\n  wmaxs = {wmaxs}")
-        print(f" normalization check\n{Pi[:,:,0]}")
+        print(f" Pi normalization check\n{Pi[:,:,0]}")
+        print(f" sigma normalization check\n{sigma.sum(-1)*(2*wmaxs)/(self.Nw*np.pi)}")
+
         if self.rescale:
             sigma = sigma_r
         if basic:
@@ -390,10 +391,15 @@ class LorentzComb(DataGenerator):
 
 
 class ContinuationData(torch.utils.data.Dataset):
-    def __init__(self, path, noise=0.0, beta=[], rescaled=False):
+    def __init__(self, path, noise=0.0, beta=[], rescaled=False, 
+                standardize=False, normalize_output=False,
+                ):
         self.x_data = np.loadtxt(open(path+"Pi.csv", "rb"), delimiter=",")
         self.y_data = np.loadtxt(open(path+"SigmaRe.csv", "rb"), delimiter=",")
+        self.N = self.y_data.shape[-1]
         self.noise = noise
+        self.standardize = standardize
+        self.normalize_output = normalize_output
         
         self.beta = beta
         if self.beta:
@@ -409,25 +415,38 @@ class ContinuationData(torch.utils.data.Dataset):
         if self.rescaled:
             scaled_path = path+"SigmaRe_scaled_4.0.csv"
             self.scaled_y_data = np.loadtxt(open(scaled_path, "rb"), delimiter=",")
-            self.wmaxs = np.loadtxt(open(path+"wmaxs.csv", "rb"), delimiter=",")
         
-        self.N = self.y_data.shape[-1]
+        self.wmaxs = np.loadtxt(open(path+"wmaxs.csv", "rb"), delimiter=",")
+
+        if self.standardize:
+            self.avg = self.x_data.mean(axis=-2)
+            self.std = self.x_data.std(axis=-2)
+            self.xTavg = self.xT_data.mean(axis=-2)
+            self.xTstd = self.xT_data.std(axis=-2)
 
     def __len__(self):
         return len(self.x_data)
 
     def __getitem__(self, index):
         if self.beta:
-            x = self.xT_data[np.random.randint(self.nT), index]
+            b = np.random.randint(self.nT)
+            x = self.xT_data[b, index]
             x += np.random.normal(0,1, size=x.shape)*self.noise
+            if self.standardize:
+                x = (x - self.xTavg[b])/self.xTstd[b]
         else:
             x = self.x_data[index] 
             x += np.random.normal(0,1, size=x.shape)*self.noise
+            if self.standardize:
+                x = (x - self.avg)/self.std
 
         if self.rescaled:
             y = self.scaled_y_data[index]
         else: 
             y = self.y_data[index]
+
+        if self.normalize_output:
+            y /= (self.N*np.pi)/(2*self.wmaxs[index])
         return x, y
 
 
@@ -440,7 +459,7 @@ def main():
         'Nwn': 128,
         'Nw': 512,
         'wmax': 20.0,
-        'beta': [4, 7, 10, 13, 16, 19, 22, 25, 28],  # 2*np.pi, # 2pi/beta = 1
+        'beta': [10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 50.0],  # 2*np.pi, # 2pi/beta = 1
         'norm': 1.0,
         'rescale': 0.0,
         # peaks

deep_continuation/train.py
@@ -17,10 +17,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
 
 try:
     import wandb
     USE_WANDB = True
+    os.environ['WANDB_IGNORE_GLOBS']="*.pt"
 except ModuleNotFoundError:
     USE_WANDB = False
 
@@ -60,7 +62,9 @@ default_parameters = {
     'cuda': True,
     'valid_fraction': 0.3,
     'rescale': False,
-    'beta': [20.0]
+    'beta': [20.0],
+    'plot': False,
+    'standardize': False,
 }
 
 help_strings = {
@@ -105,36 +109,11 @@ note: for every bool flag, an additional --no_flag is defined to turn it off.
 args = utils.parse_file_and_command(default_parameters, help_strings)
 
 
-# TODO:
-# move these two function out to utils.py and have a more general treatment
-# ideas:
-#   name(args, naming_dict)  # where naming dict specifies parameters to use
-
-def name(a=args):  # a = args
-    layers_str = str(a.layers[0])
-    for size in a.layers[1:]:
-        layers_str = layers_str + '-' + str(size)
-
-    name = f'mlp{layers_str}_{a.loss}_{a.data}n{round(a.noise,3)}_bs{a.batch_size}'
-    name += f'_lr{round(a.lr, 5)}_wd{round(a.weight_decay, 3)}'
-    name += f'_{round(a.dropout, 3)}_{a.out_unit}'
-    name += f'_bn' if a.batchnorm else ''
-    name += f'_wup' if a.warmup else ''
-    name += f'_sch{round(a.factor, 3)}-{round(a.patience, 3)}' if a.schedule else ''
-    return name
-
-
-def dump_params(args):
-    with open(f'results/params_{name(args)}.json', 'w') as f:
-        json.dump(vars(args), f, indent=4)
-
-
 class MLP(nn.Module):
     def __init__(self, args):
         super(MLP, self).__init__()
-        self.name = name(args)
         self.epoch = 0
-
+        
         self.layers = nn.ModuleList()
         sizeA = args.layers[0]
         for sizeB in args.layers[1:]:
@@ -151,9 +130,9 @@ class MLP(nn.Module):
 
         if args.out_unit == 'None':
             pass
-        elif args.out_unit == 'ReLU':
+        elif args.out_unit in ['ReLU', 'relu']:
             self.layers.append(nn.ReLU())
-        elif args.out_unit == 'Softmax':
+        elif args.out_unit in ['Softmax', 'softmax']:
             self.layers.append(nn.Softmax(dim=-1))
         # Here would be a place for our custom Softmax
         else:
@@ -177,18 +156,23 @@ def mse(outputs, targets):
     return torch.mean((outputs-targets)**2)
 
 
-def dca(outputs, targets):
+def mse_smooth(outputs, targets, factor=1):
+    ''' mean square error '''
+    return torch.mean((outputs-targets)**2) + torch.mean(factor*torch.abs(outputs[:,1:]-outputs[:,:-1]))
+
+
+def dc_absolute_error(outputs, targets):
     ''' computes the 0th component difference (DC conductivity)'''
     return torch.mean(torch.abs(outputs[:, 0]-targets[:, 0]))
 
 
-def dcs(outputs, targets):
+def dc_square_error(outputs, targets):
     ''' computes the 0th component square difference (DC conductivity)'''
     return torch.mean((outputs[:, 0]-targets[:, 0])**2)
 
 
 class Metric():
-    def __init__(self, name, data_set, loss_list=['MSE', 'mse', 'dcs', 'dca'], batch_size=512):
+    def __init__(self, name, data_set, loss_list=['mse', 'mae', 'dcs', 'dca'], batch_size=512):
         self.valid_loader = DataLoader(
             data_set, batch_size=batch_size, drop_last=True, shuffle=False)
         self.name = name
@@ -206,9 +190,9 @@ class Metric():
             elif lname == "mae":
                 self.loss[lname] = nn.L1Loss()
             elif lname == 'dcs':
-                self.loss[lname] = dcs
+                self.loss[lname] = dc_square_error
             elif lname == 'dca':
-                self.loss[lname] = dca
+                self.loss[lname] = dc_absolute_error
             elif lname == "kld":
                 self.loss[lname] = nn.KLDivLoss()
             elif hasattr(data_set, 'custom_loss'):
@@ -241,19 +225,20 @@ class Metric():
             score = self.loss_value[lname]
             if score < self.best_loss[lname]:
                 self.best_loss[lname] = score
-                self.best_model[lname] = deepcopy(model)
-                if save_best:
-                    for filename in glob(f'results/BEST_{self.name}/{lname}*_epoch*{model.name}*'):
-                        os.remove(filename)
-                    torch.save(model.state_dict(
-                    ), f'results/BEST_{self.name}/{lname}{score:.9f}_epoch{model.epoch}_{model.name}.pt')
+                self.best_model[lname] = deepcopy(model)    
+                if save_best and USE_WANDB:
+                    torch.save(
+                        model.state_dict(),
+                        os.path.join(wandb.run.dir, f"{lname}_{self.name}.pt")
+                    )
                 is_best = True
+        
         return is_best
 
     def print_results(self):
-        print(f'      {self.name}:  ', end='')
+        print(f'      {self.name}: ', end='')
         for lname in self.loss_list:
-            print(f' {self.loss_value[lname]:.9f} ({lname}) ', end='')
+            print(f' {self.loss_value[lname]:.9f} ({lname})  ', end='')
         print()
 
 
@@ -301,9 +286,11 @@ def train(args, device, train_set, valid_set, metrics=None):
     elif  args.loss == "mae":
         criterion = nn.L1Loss()
     elif  args.loss == 'dcs':
-        criterion = dcs
+        criterion = dc_square_error
     elif  args.loss == 'dca':
-        criterion = dca
+        criterion = dc_absolute_error
+    elif  args.loss == 'mss':
+        criterion = lambda o, t: mse_smooth(o, t, factor=args.smoothing)
     elif  args.loss == "kld":
         criterion = nn.KLDivLoss()
     elif hasattr(train_set, 'custom_loss'):
@@ -318,10 +305,9 @@ def train(args, device, train_set, valid_set, metrics=None):
             factor=args.factor, 
             patience=args.patience,
             verbose=True,
-            min_lr=1e-6
+            min_lr=1e-10
         )
 
-    print('training', model.name)
     early_stop_count = args.stop
     best_valid_loss = TORCH_MAX
     for epoch in range(1, args.epochs+1):
@@ -365,6 +351,14 @@ def train(args, device, train_set, valid_set, metrics=None):
 
             model.avg_valid_loss += loss.item()
             valid_n_iter += 1
+
+            if batch_number == 0 and args.plot:
+                plt.clf()
+                plt.plot(outputs[0].detach().numpy())
+                plt.plot(targets[0].detach().numpy())
+                plt.title(f"o:{outputs[0].sum().detach().numpy()}, t:{targets[0].sum().detach().numpy()}")
+                plt.pause(0.001)
+                
         
         model.avg_valid_loss = model.avg_valid_loss/valid_n_iter
         print(f'   valid loss: {model.avg_valid_loss:.9f}')
@@ -374,7 +368,7 @@ def train(args, device, train_set, valid_set, metrics=None):
             avg_valid_loss = model.avg_valid_loss
             early_stop_count = args.stop
         for metric in metric_list:
-            is_best = metric.evaluate(model, device, save_best=True, fraction=args.valid_fraction)
+            is_best = metric.evaluate(model, device, save_best=False, fraction=args.valid_fraction)
             if is_best:
                 early_stop_count = args.stop
             metric.print_results()
@@ -438,28 +432,45 @@ def main():
     if not os.path.exists('results'):
         os.mkdir('results')
 
-    train_set = data.ContinuationData(f'data/{args.data}/train/', beta=args.beta, noise=args.noise, rescaled=args.rescale)
-    valid_set = data.ContinuationData(f'data/{args.data}/valid/', beta=args.beta, noise=args.noise, rescaled=args.rescale)
+
+    norm_out = 
+    
+    train_set = data.ContinuationData(
+        f'data/{args.data}/train/',
+        beta=args.beta,
+        noise=args.noise,
+        rescaled=args.rescale,
+        standardize=args.standardize,
+        normalize_output=norm_out,
+    )
+    valid_set = data.ContinuationData(
+        f'data/{args.data}/valid/',
+        beta=args.beta,
+        noise=args.noise,
+        rescaled=args.rescale,
+        standardize=args.standardize,
+        normalize_output=norm_out,
+    )
 
     # VALID LIST
     path_dict = {
         # 'F': 'data/Fournier/valid/',
-        'G': 'data/G1/valid/',
-        'B': 'data/B1/valid/',
+        # 'G': 'data/G1/valid/',
+        # 'B': 'data/B1/valid/',
     }
     scale_dict = {
-        'N': False,
-        'R': True
+        # 'N': False,
+        # 'R': True
     }
     noise_dict = {
         # '0': 0,
-        '5': 1e-5,
+        # '5': 1e-5,
         # '3': 1e-3,
         # '2': 1e-2,
     }
     beta_dict = {
         # 'T10': [10.0],
-        'T20': [20.0],
+        # 'T20': [20.0],
         # 'T30': [30.0],
         # 'T35': [35.0],
         # 'l3T': [15.0, 20.0, 25.0], 
@@ -471,7 +482,14 @@ def main():
         for s, scale in scale_dict.items():
             for b, beta, in beta_dict.items():
                 for n, noise in noise_dict.items():
-                    metrics_dict[p+n+b+s] = data.ContinuationData(path, noise=noise, beta=beta, rescaled=scale)
+                    metrics_dict[p+n+b+s] = data.ContinuationData(
+                        path,
+                        noise=noise,
+                        beta=beta,
+                        rescaled=scale,
+                        standardize=args.standardize,
+                        normalize_output=norm_out,
+                    )
 
 
     for metric in metrics_dict: